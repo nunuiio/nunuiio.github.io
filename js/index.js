1ba5f44f3f9011d958b1ca8bb5ebe0c65ff53172
nunuiio <loelee91@gmail.com>
2022-01-01 20:59:27 +0900

    modi

--- js/index.js
+++ js/index.js
@@ -28,11 +28,11 @@ $('.close').click(function(){
 // })
 
 
-$('.main-nav li a').each(function(index){
-  $(this).attr('menu-index', index);
-}).click(function(){
-  var index = $(this).attr('menu-index');
-  $('.main-nav li a[menu-index=' + index + ']').addClass('on');
-  $('.main-nav li a[menu-index!=' + index + ']').removeClass('on');
-});
+// $('.main-nav li a').each(function(index){
+//   $(this).attr('menu-index', index);
+// }).click(function(){
+//   var index = $(this).attr('menu-index');
+//   $('.main-nav li a[menu-index=' + index + ']').addClass('on');
+//   $('.main-nav li a[menu-index!=' + index + ']').removeClass('on');
+// });
 
