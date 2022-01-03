87a480247181d6fed698d3d515853ba2a1f6b7a4
nunuiio <loelee91@gmail.com>
2022-01-03 13:49:44 +0900

    slidemodi

--- js/index.js
+++ js/index.js
@@ -67,14 +67,14 @@ $('.close').click(function () {
 						breakpoint: 960, //화면 사이즈 960px
 						settings: {
 							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:3 
+							slidesToShow:1 
 						} 
 					},
 					{ 
 						breakpoint: 768, //화면 사이즈 768px
 						settings: {	
 							//위에 옵션이 디폴트 , 여기에 추가하면 그걸로 변경
-							slidesToShow:2 
+							slidesToShow:1 
 						} 
 					}
 				]
